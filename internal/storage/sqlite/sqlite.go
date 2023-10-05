7faa1e5a0f3f8911c9e7fc530f082fc8bf5031d9
f1xend <sor1997@yandex.ru>
2023-10-05 18:50:42 +0300

    end with storage

--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -4,7 +4,8 @@ import (
 	"database/sql"
 	"fmt"
 
-	_ "github.com/mattn/go-sqlite3"
+	"github.com/f1xend/URL_shortener/internal/storage"
+	"github.com/mattn/go-sqlite3"
 )
 
 type Storage struct {
@@ -38,4 +39,29 @@ func New(storagePath string) (*Storage, error) {
 	}
 
 	return &Storage{db: db}, nil
+}
+
+func (s *Storage) SaveURL(urlToSave string, alias string) (int64, error) {
+	const op = "storage,sqlite,SaveURL"
+
+	stmt, err := s.db.Prepare("INSERT INTO url(url, alias) values(?,?)")
+	if err != nil {
+		return 0, fmt.Errorf("%s, %w", op, err)
+	}
+
+	res, err := stmt.Exec(urlToSave, alias)
+	if err != nil {
+		// TODO: refactor this
+		if sqliteErr, ok := err.(sqlite3.Error); ok && sqliteErr.ExtendedCode == sqlite3.ErrConstraintUnique {
+			return 0, fmt.Errorf("%s, %w", op, storage.ErrURLExists)
+		}
+
+		return 0, fmt.Errorf("%s, %w", op, err)
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("%s, %w", op, err)
+	}
+	return id, nil
 }
