de7972b34f3e815d286c234aa5c75f19d8e5b69d
f1xend <sor1997@yandex.ru>
2023-10-05 09:16:29 +0300

    init loggeer: slog

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,17 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"os"
+
+	"log/slog"
 
 	"github.com/f1xend/URL_shortener/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
 
-	// TODO: init loggeer: slog
+	log.Info("starting url-shorter", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 
 	// TODO: init storage: sqlite
 
@@ -19,4 +28,25 @@ func main() {
 
 	// TODO: run server:
 
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
